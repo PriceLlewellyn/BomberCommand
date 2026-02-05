272c335f3d1b6847cc223b35f3a6aaff7ab1a6c7
Llewellyn Price <llewellyn.price@Llewellyns-MacBook-Pro.local>
2026-02-05 18:38:22 +0000

    board only being displayed for my fleet not the enemy

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,19 @@ const rotate = () => {
 
 // Run rotate when the button is clicked
 document.getElementById("rotate")!.addEventListener("click", rotate)
+
+//board
+const createGrid = (element: HTMLElement) => {
+  for (let i = 0; i < 100; i++) {
+    const cell = document.createElement("div")
+    cell.classList.add("cell");
+    cell.id = `cell-${i}`
+    element.append(cell)
+  }
+}
+
+const playerBoard = document.getElementById("player")!
+createGrid(playerBoard)
+
+const computerBoard = document.getElementById("computer")!
+createGrid(computerBoard)
