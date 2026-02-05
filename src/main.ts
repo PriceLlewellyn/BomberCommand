80a1b47cf077b37ab3e25edda1bf5c62b8119ef7
Llewellyn Price <llewellyn.price@Llewellyns-MacBook-Pro.local>
2026-02-05 17:48:34 +0000

    logic to flip the ships developed

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import './style.css'
 
+// Tracks whether ships are rotated
+let isFlipped = false;
 
+// Tracks whether ships are rotated
+const rotate = () => {
+  const shipHolder = document.getElementById("ships")!
+  const ships = [...shipHolder.children] as HTMLElement[]
 
+  if (!isFlipped){
+    ships.forEach(ship => ship.classList.add("rotated"));
+  } else {
+    ships.forEach((ship) => ship.classList.remove("rotated"));
+  }
+   // Switch state for next click
+  isFlipped = !isFlipped;
+};
+
+// Run rotate when the button is clicked
+document.getElementById("rotate")!.addEventListener("click", rotate)
