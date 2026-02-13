16c014e8e9fda7628ee1dbae1c9a7cc6fefbc63d
Llewellyn Price <llewellyn.price@Llewellyns-MacBook-Pro.local>
2026-02-13 16:30:29 +0000

    added drag functions, bug- not showing highlighted cells

--- src/main.ts
+++ src/main.ts
@@ -56,25 +56,34 @@ const cruiser = new Ship("cruiser", 3);
 const battleship = new Ship("battleship", 4);
 const carrier = new Ship("carrier", 5);
 
+const getShipCells = (size: number, x: number, y: number, isFlipped: boolean, player: string) => {
+  const board = player === "computer" ? computerBoard : playerBoard
+
+  const cells = (Array.from(board.getElementsByClassName("cell"))) as HTMLElement[]
+
+  const spots = new Array(size).fill(-1).map((_, i) => {
+    if(isFlipped) {
+      return x * 10 + y + i * 10;
+    } else {
+      return x * 10 + y + i;
+    }
+  })
+  .map(i => cells[i])
+
+  return spots
+}
+
 const addShipToGrid = (ship: Ship) => {
-  // Get all cells on the computer board
-  const cells = Array.from(computerBoard.getElementsByClassName("cell")) as HTMLElement[]
 
   while(true) {
     // Randomly choose horizontal or vertical
-    const horizontal = Math.random() < .5
+    const isFlipped = Math.random() < .5
     // Pick a starting position that keeps the ship in bounds
-    const x = horizontal ? Math.floor(Math.random() * 10) : Math.floor(Math.random() * (10 - ship.size))
-    const y = horizontal ? Math.floor(Math.random() * (10 - ship.size)) : Math.floor(Math.random() * 10)
+    const y = isFlipped ? Math.floor(Math.random() * 10) : Math.floor(Math.random() * (10 - ship.size))
+    const x = isFlipped ? Math.floor(Math.random() * (10 - ship.size)) : Math.floor(Math.random() * 10)
 
     // Calculate the cell indexes the ship will occupy
-    const spots = new Array(ship.size).fill(-1).map((_, i) => {
-      if(horizontal) {
-        return x * 10 + y + i;
-      } else {
-        return x * 10 + y + i * 10;
-      }
-    }).map(i => cells[i])
+    const spots = getShipCells(ship.size, x, y, isFlipped, "computer")
 
     // check if all cells are free then place ship
     if (spots.every(spot => !spot.classList.contains('taken'))){
@@ -87,17 +96,54 @@ const addShipToGrid = (ship: Ship) => {
 const allShips = [destroyer, submarine, cruiser, battleship, carrier];
 allShips.forEach(ship => addShipToGrid(ship));
 
-// Drag ships
-// When you click a ship
+
+
+const checkValidTarget = (id:number) => {
+    const size = allShips.find(ship => ship.name === draggingShip)?.size || 0;
+    const x = Math.floor(id/ 10);
+    const y = id - x * 10;
+
+
+    if (!isFlipped) {
+      if (size + y > 10) return false;
+      return true;
+    } else {
+      if (size + x > 10) return false;
+      return true;
+    }
+};
+
+const highlightCells = (id: number) => {
+    const size = allShips.find(ship => ship.name === draggingShip)?.size || 0;
+    const x = Math.floor(id/ 10);
+    const y = id - x * 10;
+
+    const validCells = getShipCells(size,x,y,isFlipped, "player")
+    validCells.forEach(cell => cell.classList.add("highlight"))
+}
+
 const ondragstart = (e : MouseEvent) => {
   console.log(e.target)
 };
 
+let draggingShip = ''
+const onDragStart = (e: MouseEvent) => {
+  draggingShip = (e.target as HTMLElement).classList[0]
+}
+
 // When you drag over a cell
 const onDragOver = (e : DragEvent) => {
-  console.log(e.target)
-  e.preventDefault();
+  const targetID = (e.target as HTMLElement).id;
+  const id = Number(targetID.split("cell-")[1]);
+  const isValidDrop = checkValidTarget(id);
+  if (isValidDrop) {
+    highlightCells(id);
+  }
 };
+
+const OnDragLeave = (E: DragEvent) => {
+  myCells.forEach ((cell)=> cell.classList.remove("highlight")) 
+}
 
 // When you drop on a cell
 const onDrop = (e : DragEvent) => {
@@ -109,6 +155,7 @@ const myCells = Array.from(playerBoard.getElementsByClassName("cell")) as HTMLEl
 // Add drag events to each cell
 myCells.forEach(cell => {
   cell.addEventListener("dragover", onDragOver)
+  cell.addEventListener("dragleave", OnDragLeave)
   cell.addEventListener("drop", onDrop)
 })
 
